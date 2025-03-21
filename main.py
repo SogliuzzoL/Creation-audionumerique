53b0b2424a91d2f14476e19e60131980efc680b7
SOGLIUZZO Loris <sogliuzzo.loris@gmail.com>
2025-03-21 10:08:05 +0100

    buttons sont clickables

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from pythonosc.udp_client import SimpleUDPClient
 import pygame
 
+
+def fxChangeValue(trackId, fxId, fxParamId, value):
+    client.send_message(f"/track/{trackId}/fx/{fxId}/fxparam/{fxParamId}/value", value)
+
+
+def trackChangeVolume(trackId, volume):
+    client.send_message(f"/track/{trackId}/volume", volume)
+
+
 ip = "127.0.0.1"
 port = 8000
 
-track_id = 1
-fx_id = 1
-fxparam_id = 2
-volume = 0.5
-value = 1.5
+defaultValue = 0.5
+trackId = 1
 
 client = SimpleUDPClient(ip, port)
-client.send_message(f"/track/{track_id}/volume", volume)
-for fxparam_id in range(1, 12):
-    client.send_message(
-        f"/track/{track_id}/fx/{fx_id}/fxparam/{fxparam_id}/value", value
-    )
+trackChangeVolume(trackId, defaultValue)
 
 pygame.init()
 window_width = 720
@@ -31,26 +33,37 @@ y_start = 0.05 * window_height
 width = (window_width - 2 * x_start - n_colums * margin) / n_colums
 height = (window_height - 2 * y_start - n_row * margin) / n_row
 font = pygame.font.SysFont(pygame.font.get_fonts()[1], 24)
+buttons = []
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            for i, button in enumerate(buttons):
+                if button.collidepoint(pygame.mouse.get_pos()):
+                    print(f"Button {i + 1} clicked")
+                    if i == 0:
+                        fxChangeValue(1, 1, 1, 0.4)
+
+    buttons = []
     screen.fill("white")
     for i in range(n_colums):
         for j in range(n_row):
-            pygame.draw.rect(
-                screen,
-                "black",
-                (
-                    x_start + i * (width + margin),
-                    y_start + j * (height + margin),
-                    width,
-                    height,
-                ),
+            buttons.append(
+                pygame.draw.rect(
+                    screen,
+                    "black",
+                    (
+                        x_start + i * (width + margin),
+                        y_start + j * (height + margin),
+                        width,
+                        height,
+                    ),
+                )
             )
-            text = font.render(str(i + j * n_row + 1), True, "white")
+            text = font.render(str(i * n_colums + j + 1), True, "white")
             screen.blit(
                 text,
                 (
